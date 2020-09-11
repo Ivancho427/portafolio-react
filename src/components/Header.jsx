37ea0bd797d06d795216c81c6a5c2b8c36cd2f87
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-09-11 16:03:56 -0500

    Cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import ImagenHeader from '.././imagenes/Header.jpg'
+import { Card } from 'react-bootstrap'
 
 function Header() {
     return (
 
-        <section className="header" style={{backgroundColor: "mintcream"}}>
-            <div id="Container-img">
-                <img style={{ border: "2px solid firebrick", opacity:"0.7" }} className="imagen-header w-100 h-50" id="img" src={ImagenHeader} alt="Foto Header" />
-            </div>           
+         <section className="header" style={{ opacity:"0.8"}}>          
+            <Card.Body className="desarrollador_web " style={{height: "400px"}}></Card.Body>
+            <Card.Body className="texto-desa mb-3 ml-5" style={{ color: "white", textAlign:"initial" , opacity: "0.7"}} ><h1><span><strong><em>Desarrollador Web</em></strong></span></h1>
+            <h2 className="texto-desa mb-5 "><span><em>Bienvenido a mi portafólio</em></span></h2>
+            </Card.Body>
+            
         </section>
     )
 }
 
 export default Header
+
+
 
 
 
