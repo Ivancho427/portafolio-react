2dfe4e926e5c27166c9ff327cef951f736845eef
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-09-11 12:44:00 -0500

    Cambios y Actualización Responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
 import CV from '.././imagenes/CV Iván Darío Mejía Muñoz.pdf'
 
 
@@ -11,7 +10,6 @@ function Footer() {
         <a style={{ backgroundColor: "white", color: "firebrick" }} className="boton_portafolio mt-5 mb-3" href="https://github.com/Ivancho427" target="_blank" rel="noopener noreferrer" id="proyectos"><h5>TODOS LOS PROYECTOS</h5></a><br/>
         <a style={{ backgroundColor: "white", color: "firebrick" }} id="boton-cv" href={CV} target="_blank" rel="noopener noreferrer" size="sm" ><h5>CV  - IVÁN DARÍO MEJÍA MUÑOZ</h5></a>   
 </section>
-
 
     )
 }
