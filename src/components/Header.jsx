7a68c1a0e4b272cd33e455945f48a105b48041ab
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-09-15 11:19:21 -0500

    Cambios Finales

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-// import ImagenHeader from '.././imagenes/Header.jpg'
 import ImagenFotoHV from '.././imagenes/Foto HV.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faHtml5,faJs, faReact } from '@fortawesome/free-brands-svg-icons'
 
 function Header() {
     return (
@@ -14,7 +15,11 @@ function Header() {
                     <h2 className="nombre mt-5">Iván Darío Mejía Muñoz</h2><hr/>
                     <h4 className="texto-desarrollador mt-4">Desarrollador Web</h4>
                     <p>Desarrollador Frontend con énfasis en Node/ReactJS - Ingeniero de Telecomunicaciones.<br/>
-                    Apasionado por la tecnología y el mundo digital.</p><hr/> 
+                    Apasionado por la tecnología y el mundo digital.</p><hr/>                     
+                    <FontAwesomeIcon icon={faHtml5} style={{color:"#9b0909", fontSize:"35", marginLeft:"3"}} />
+                    <FontAwesomeIcon icon={faCss3} style={{color:"#9b0909", fontSize:"35", marginLeft:"15"}} />
+                    <FontAwesomeIcon icon={faJs} style={{color:"#9b0909", fontSize:"35", marginLeft:"15"}} />
+                    <FontAwesomeIcon icon={faReact} style={{color:"#9b0909", fontSize:"35", marginLeft:"15"}} />
                         
                 </div>  
                 </article>
